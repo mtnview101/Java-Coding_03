d59e5ec783c2efcfbf78ff3682fafcca688399fb
mtnview101 <mtnview101@gmail.com>
2017-01-02 18:43:57 -0800

    longest string 2/1 commit

--- src/Longest_String.java
+++ src/Longest_String.java
@@ -1,7 +1,7 @@
 
 public class Longest_String {
 public static void main(String[] args) {
-	String input="Cheese Pepperoni Olives Pizza Pepperoni Cheese Pepperoni Nakhabtsev HAPPY_NEW_YEAR_2017";
+	String input="Cheese Pepperoni Olives Pizza Pepperoni Cheese Pepperoni Nakhabtsev HAPPY_NEW_YEAR_2017 I_love_California";
 	String[] a=input.split(" ");
 	int maxLength=0;
 	String longestString=null;
